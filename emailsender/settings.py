64782b56254c1253535a1a391327cab923e37bff
Bhushan Chaudhari <68490983+bhush-n@users.noreply.github.com>
2024-01-18 09:34:33 +0530

    Update settings.py

--- emailsender/settings.py
+++ emailsender/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-^5vjeugq4u!s!fen2hy2q@pxn8jo*)ww*f-!zw4lejn1iskmln
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.vercel.app']
 
 
 # Application definition
